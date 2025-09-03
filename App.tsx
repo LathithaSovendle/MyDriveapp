6c8cd0173fcd13ab90b35f5f2d810371e398b312
LathithaSovendle <st10476533@vcconnect.edu.za>
2025-09-03 14:54:14 +0200

    We have added licenceyears

--- App.tsx
+++ App.tsx
@@ -3,21 +3,28 @@ import { StyleSheet, Text, View, Button  } from 'react-native';
 
 export default function App() {
 const age = 16; 
+const Livenceyears = 0;
 let feedback = '';
 
-if (age >= 18) {
- feedback = 'You are allowed to drive.YAY!'
-}
-else{
-feedback = 'you are not allowed to drive yet.'
-}
+if (age >= 18)
+    if (Livenceyears >= 3){
+       feedback = 'You are a qualified, safe driver.';
+    }
+   else
+    {
+       feedback = 'You are allowed to drive.YAY!'
+    }
+else
+   { 
+      feedback = 'you are not allowed to drive yet.'
+   }
+
 return (
     <View style={styles.container}>
     {/*we use one bracket*/}
       <Text style = {styles.response}>
         {/*This is my message to be displayed to the user*/}
         {feedback}</Text>
-      
     </View>                                                                                          
   );
 }
@@ -25,12 +32,14 @@ return (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   response:{
     fontSize: 38,
+    color: 'white',
+  
   }
 });
