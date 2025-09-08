2e6e690b2b8f150492515409984eb823e8304e16
LathithaSovendle <st10476533@vcconnect.edu.za>
2025-09-08 14:31:58 +0200

    Added driving Rank message using switch statements

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ const age = 20;
 const Licenceyears = 5;
 let feedback = '';
 
+
 if (age >= 18)
+/*the condition (age >= 18) was true*/
     if (Licenceyears >= 3){
        feedback = 'You are a qualified, safe driver.';
     }
@@ -14,24 +16,60 @@ if (age >= 18)
     {
        feedback = 'You are allowed to drive.YAY!'
     }
+    /*the condition (age >= 18) was false*/
 else
    { 
       feedback = 'you are not allowed to drive yet.'
    }
+   //Adding a SWITCH STATEMENT 
+  let drivingRank = '';
+  switch (Licenceyears) {
+     case 0:
+      drivingRank='Newbie driver';
+      break;
+      case 1: 
+      drivingRank = 'Rookie with 1 year of driving experience';
+       break;      
+       case 2: 
+       drivingRank = 'Intermediate driver with 2 years of driving experience';
+        break; 
+        case 3: 
+        case 4:
+       drivingRank = 'Experienced driver';
+       break; 
+       default:'Vetean driver with many years of experience';
+  }
+
+
 return ( 
   //we use safe area view to
   <SafeAreaView style={styles.container}>
     <View style ={styles.content}>
       {/*Add a custom and other text component specific styles to the heading*/}
-      <Text style = {[styles.response,{fontSize:40, color:'#072F5F', fontStyle:'italic', marginBottom:5, textAlign:'center'}]}>Driving Licence Checker</Text>         
+      <Text style = {[styles.response,{fontSize:24, color:'#072F5F', fontStyle:'italic', marginBottom:5, textAlign:'center'}]}>Driving Licence Checker</Text>         
      <Image source ={{uri:"https://cbx-prod.b-cdn.net/COLOURBOX28085854.jpg?width=1600&height=1600&quality=90",}}
         style = {styles.image}/>
-        <Text style={styles.response}>Age:{age}</Text> 
+        <Text style={styles.response}>Age:{age}
+          </Text> 
         <Text style={styles.response}>Licenceyears:{Licenceyears}</Text>
     {/*we use one bracket*/}
       <Text style = {[styles.response, {color:'green'}]}>
         {/*This is my message to be displayed to the user*/}
         {feedback}
+        {/*Copy image address*/}
+        </Text>
+        {/*Add a custom and other text component specific styles to the heading*/}
+      <Text style = {[styles.response,{fontSize:40, color:'#072F5F', fontStyle:'italic', marginBottom:5, textAlign:'center'}]}>Driving Licence Checker</Text>         
+     <Image source ={{uri:"https://cbx-prod.b-cdn.net/COLOURBOX28085854.jpg?width=1600&height=1600&quality=90",}}
+        style = {styles.image}/>
+        <Text style={styles.response}>Age:{age}
+          </Text> 
+
+        <Text style={styles.response}>Licenceyears:{Licenceyears}</Text>
+    {/*we use one bracket*/}
+      <Text style = {[styles.response, {color:'green'}]}>
+        {/*This is a driving rank Text Component*/}
+        Driving Rank: {drivingRank}
         {/*Copy image address*/}
         </Text>
     </View>    
@@ -45,17 +83,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
   content:{
    width:'100%',
    alignItems:'center'
   },
-
   response:{
     fontSize: 28,
     color: 'Black',
     marginVertical:30 //this is for splace between the image and text we use marginTop or bottom
-    
   },
   image:{
     width:'100%',
